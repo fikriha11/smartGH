a3bb49c5fddcc1f4fc17200d32cbd899267894d0
fikriha11 <fikrihaikal568@gmail.com>
2021-11-22 12:02:29 +0700

    add file sht13.py

--- smartGH.py
+++ smartGH.py
@@ -49,25 +49,6 @@ def readDHT():
         print("Sensor DHT error")
 
 
-def readSHT():
-    # Get I2C bus
-    bus = smbus.SMBus(1)
-    bus.write_i2c_block_data(0x44, 0x2C, [0x06])
-    time.sleep(0.5)
-    data = bus.read_i2c_block_data(0x44, 0x00, 6)
-
-    # Convert the data
-    temp = data[0] * 256 + data[1]
-    cTemp = -45 + (175 * temp / 65535.0)
-    fTemp = -49 + (315 * temp / 65535.0)
-    humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
-
-    # Output data to screen
-    print("Temperature in Celsius is : %.2f C" % cTemp)
-    print("Temperature in Fahrenheit is : %.2f F" % fTemp)
-    print("Relative Humidity is : %.2f %%RH" % humidity)
-
-
 def ReadSensor():
     try:
         readDHT()
@@ -84,9 +65,7 @@ def ReadSensor():
 
 
 while True:
-    # ReadSensor()
-    # if(time() - SoundTime) > 30:
-    #     soundOutput()
-    #     SoundTime = time()
-
-    readSHT()
+    ReadSensor()
+    if(time() - SoundTime) > 30:
+        soundOutput()
+        SoundTime = time()
